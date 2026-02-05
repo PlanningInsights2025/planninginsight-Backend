d805647dd69d3b5c6c23698cdf39f1899b9a601d
Manish Gavali <manishgavali186@gmail.com>
2026-02-05 12:22:17 +0530

    Add database connection to Vercel serverless handler

--- api/index.js
+++ api/index.js
@@ -1,3 +1,19 @@
 import app from '../src/app.js'
+import { connectDB } from '../src/config/database.js'
 
-export default app
+let isConnected = false
+
+export default async (req, res) => {
+  // Connect to database if not already connected
+  if (!isConnected) {
+    try {
+      await connectDB(process.env.MONGODB_URI)
+      isConnected = true
+    } catch (error) {
+      console.error('Database connection error:', error)
+      // Continue anyway - some routes don't need DB
+    }
+  }
+  
+  return app(req, res)
+}
